15bf3c8b507635e36b6a4a6c7348990a5fc84aa2
Ezra Anglo <dev.ezraanglo@gmail.com>
2022-07-01 18:19:42 -0500

    Fixed uppercase issue and username tag error log

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -106,7 +106,7 @@ module.exports = (client, message) => {
         //     // });
         // }
 
-        const args = message.content.slice(prefix.length).trim().split(/ +/g);
+        const args = message.content.toLowerCase().slice(prefix.length).trim().split(/ +/g);
         const command = args.shift().toLowerCase();
 
         const cmd = client.commands.get(command) || client.commands.find(cmd => cmd.aliases && cmd.aliases.includes(command));
