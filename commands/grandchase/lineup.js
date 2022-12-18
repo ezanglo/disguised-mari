caaacac63ca0ccfe20eff4ad3db865512b0a03a6
Fallenthetaco <nghjame62@gmail.com>
2022-12-18 09:01:54 -0800

    Made small fixes along with moving the if statement to a new function

--- commands/grandchase/lineup.js
+++ commands/grandchase/lineup.js
@@ -51,7 +51,7 @@ module.exports = {
 
         if (data.pageInfo.totalRows > 1) {
 
-          const lineup = await this.getContentLineup(data.list, interaction);
+          const lineup = await this.getContentOverall(data.list, interaction);
 
           await interaction.editReply({
             embeds: lineup
@@ -64,7 +64,7 @@ module.exports = {
               embeds: [
                 new EmbedBuilder({
                   color: 0xed4245,
-                  description: `Content not found ${interaction.user}... try again ? ❌`,
+                  description: `Lineup not found ${interaction.user}... try again ? ❌`,
                 }),
               ],
             });
@@ -79,7 +79,7 @@ module.exports = {
             embeds: [
               new EmbedBuilder({
                 color: 0xed4245,
-                description: `Content not found ${interaction.user}... try again ? ❌`,
+                description: `Lineup not found ${interaction.user}... try again ? ❌`,
               }),
             ],
           });
@@ -98,252 +98,250 @@ module.exports = {
         interaction.client.errorLog(e, interaction);
       });
   },
-  async getContentLineup(data, interaction, refreshImage) {
+  async getContentOverall(data, interaction) {
+    const embed = new EmbedBuilder()
+      .setThumbnail(data[0].ContentTypeRead.Icon)
+      .setTitle(data[0].ContentTypeRead.Name)
 
-    if (Array.isArray(data)) {
-      const embed = new EmbedBuilder()
-        .setThumbnail(data[0].ContentTypeRead.Icon)
-        .setTitle(data[0].ContentTypeRead.Name)
+    for (var i = 0; i < data.length; i++) {
 
-      for (var i = 0; i < data.length; i++) {
-
-        if (data[i].PartySkills.length != 1) {
-          embed.addFields([
-            {
-              name: `Phase ${i + 1} ${data[i].ContentPhaseRead.AttributeTypeRead.DiscordEmote}`,
-              value: data[i].Heroes.map(
-                (h) =>
-                  `${h.DiscordEmote}`
-              )
-                .join("") + " " + data[i].PartySkills.map(x => x.Name).join("/") + " " + data[i].HeroPetRead.Name
-            }
-          ]);
-        } else {
-          embed.addFields([
-            {
-              name: `Phase ${i + 1} ${data[i].ContentPhaseRead.AttributeTypeRead.DiscordEmote}`,
-              value: data[i].Heroes.map(
-                (h) =>
-                  `${h.DiscordEmote}`
-              )
-                .join("") + " " + data[i].PartySkills[0].Name + " " + data[i].HeroPetRead.Name
-            }
-          ]);
-        }
-      }
-      let embeds = [];
-      embeds.push(embed);
-
-      return embeds;
-    } else {
-      const content = data.ContentTypeRead.Name;
-      const phase = data.ContentPhaseRead.Name;
-      const lineupDate = data.UpdatedAt ? data.UpdatedAt : data.CreatedAt;
-
-      const embed = new EmbedBuilder()
-        .setThumbnail(data.ContentTypeRead.Icon)
-        .setFooter({
-          text: `Last updated ${new Date(lineupDate).toLocaleDateString()}`,
-        })
-        .addFields([
+      if (data[i].PartySkills.length != 1) {
+        embed.addFields([
           {
-            name: `${content} - ${phase}`,
-            value: `${data.ContentPhaseRead.AttributeTypeRead.DiscordEmote} ${data.ContentPhaseRead.Description}`,
-            inline: true,
-          },
+            name: `Phase ${i + 1} ${data[i].ContentPhaseRead.AttributeTypeRead.DiscordEmote}`,
+            value: data[i].Heroes.map(
+              (h) =>
+                `${h.DiscordEmote}`
+            )
+              .join("") + " " + data[i].PartySkills.map(x => x.Name).join("/") + " " + data[i].HeroPetRead.Name
+          }
         ]);
-
-      if (data.ContentPhaseRead.Enemies) {
-        let value = data.ContentPhaseRead.Enemies.value.join(", ");
-        if (data.ContentPhaseRead.Enemies.type == "heroes") {
-          value = client.heroes
-            .filter((h) => data.ContentPhaseRead.Enemies.value.includes(h.Code))
-            .map((x) => x.DiscordEmote)
-            .join(" ");
-        }
-        embed.addFields([{ name: "Enemies", value: value, inline: true }]);
+      } else {
+        embed.addFields([
+          {
+            name: `Phase ${i + 1} ${data[i].ContentPhaseRead.AttributeTypeRead.DiscordEmote}`,
+            value: data[i].Heroes.map(
+              (h) =>
+                `${h.DiscordEmote}`
+            )
+              .join("") + " " + data[i].PartySkills[0].Name + " " + data[i].HeroPetRead.Name
+          }
+        ]);
       }
+    }
+    let embeds = [];
+    embeds.push(embed);
 
-      // if(data.Video){
-      //   const videoParts = data.Video.split('/');
-      //   const videoId = videoParts[videoParts.length -1];
-      //   embed.setImage(`https://img.youtube.com/vi/${videoId}/maxresdefault.jpg`);
-      // }
-      embed.addFields([
-        { name: "Gameplay", value: data.Video ? data.Video : "*placeholder*" },
+    return embeds;
+  },
+  async getContentLineup(data, interaction, refreshImage) {
+    const content = data.ContentTypeRead.Name;
+    const phase = data.ContentPhaseRead.Name;
+    const lineupDate = data.UpdatedAt ? data.UpdatedAt : data.CreatedAt;
+
+    const embed = new EmbedBuilder()
+      .setThumbnail(data.ContentTypeRead.Icon)
+      .setFooter({
+        text: `Last updated ${new Date(lineupDate).toLocaleDateString()}`,
+      })
+      .addFields([
+        {
+          name: `${content} - ${phase}`,
+          value: `${data.ContentPhaseRead.AttributeTypeRead.DiscordEmote} ${data.ContentPhaseRead.Description}`,
+          inline: true,
+        },
       ]);
 
-      const attributes = {
-        red: { count: 0 },
-        green: { count: 0 },
-        blue: { count: 0 },
-        light: { count: 0 },
-        dark: { count: 0 },
-      };
+    if (data.ContentPhaseRead.Enemies) {
+      let value = data.ContentPhaseRead.Enemies.value.join(", ");
+      if (data.ContentPhaseRead.Enemies.type == "heroes") {
+        value = client.heroes
+          .filter((h) => data.ContentPhaseRead.Enemies.value.includes(h.Code))
+          .map((x) => x.DiscordEmote)
+          .join(" ");
+      }
+      embed.addFields([{ name: "Enemies", value: value, inline: true }]);
+    }
 
-      data.Heroes.forEach((hero) => {
-        if (hero.AttributeTypeRead.Code in attributes) {
-          attributes[hero.AttributeTypeRead.Code].count += 1;
-          attributes[hero.AttributeTypeRead.Code].emote =
-            hero.AttributeTypeRead.DiscordEmote;
+    // if(data.Video){
+    //   const videoParts = data.Video.split('/');
+    //   const videoId = videoParts[videoParts.length -1];
+    //   embed.setImage(`https://img.youtube.com/vi/${videoId}/maxresdefault.jpg`);
+    // }
+    embed.addFields([
+      { name: "Gameplay", value: data.Video ? data.Video : "*placeholder*" },
+    ]);
+
+    const attributes = {
+      red: { count: 0 },
+      green: { count: 0 },
+      blue: { count: 0 },
+      light: { count: 0 },
+      dark: { count: 0 },
+    };
+
+    data.Heroes.forEach((hero) => {
+      if (hero.AttributeTypeRead.Code in attributes) {
+        attributes[hero.AttributeTypeRead.Code].count += 1;
+        attributes[hero.AttributeTypeRead.Code].emote =
+          hero.AttributeTypeRead.DiscordEmote;
+      }
+    });
+
+    const bonuses = [];
+    for (const key in attributes) {
+      if (attributes[key].count >= 2) {
+        bonuses.push(
+          `${attributes[key].emote} ${(attributes[key].count - 1) * 10}%`
+        );
+      }
+    }
+
+    const halfIndex = Math.ceil(data.Heroes.length / 2);
+    embed.addFields([
+      {
+        name: "Heroes",
+        value: data.Heroes.slice(0, halfIndex)
+          .map(
+            (h) =>
+              `${h.HeroClassRead.DiscordEmote}${h.AttributeTypeRead.DiscordEmote} ${h.DiscordEmote} ${h.DisplayName}`
+          )
+          .join("\n"),
+        inline: true,
+      },
+      {
+        name: "\u200b",
+        value: data.Heroes.slice(halfIndex)
+          .map(
+            (h) =>
+              `${h.HeroClassRead.DiscordEmote}${h.AttributeTypeRead.DiscordEmote} ${h.DiscordEmote} ${h.DisplayName}`
+          )
+          .join("\n"),
+        inline: true,
+      },
+      {
+        name: "Bonus",
+        value: bonuses.length > 0 ? bonuses.join(" ") : "None",
+        inline: true,
+      },
+      {
+        name: "Pet",
+        value: data.HeroPetRead.Name ? data.HeroPetRead.Name : "None",
+      },
+    ]);
+
+    if (data.Frequency) {
+      const numbers = [
+        "one",
+        "two",
+        "three",
+        "four",
+        "five",
+        "six",
+        "seven",
+        "eight",
+      ];
+      const rows = [];
+      let i = 0;
+      data.Frequency.Skills.forEach((skill) => {
+        const skillInfo = skill.Code.split(".");
+        const heroEmote = data.Heroes.find(
+          (h) => h.Code == skillInfo[0]
+        ).DiscordEmote;
+        let skillEmote = "";
+        switch (skillInfo[1]) {
+          case "s1":
+            skillEmote =
+              skill.Frequency != 0
+                ? "<:s1:1044371160168661032>"
+                : "<:s1_disabled:1044373922042347560>";
+            if (skillInfo[2] == "lb") {
+              skillEmote =
+                skill.Frequency != 0
+                  ? "<:s1lb:1044371161334677544>"
+                  : "<:s1lb_disabled:1044373921211879424>";
+            }
+            break;
+          case "s2":
+            skillEmote =
+              skill.Frequency != 0
+                ? "<:s2:1044371163113074779>"
+                : " <:s2_disabled:1044373918867263508>";
+            if (skillInfo[2] == "lb") {
+              skillEmote =
+                skill.Frequency != 0
+                  ? "<:s2lb:1044371162299367464>"
+                  : "<:s2lb_disabled:1044373920217833572>";
+            }
+            break;
         }
+        skill.Frequency = parseFloat(skill.Frequency).toFixed(1);
+        if (skill.Heal) {
+          skill.Frequency = `${skill.Frequency * 100}%`;
+        }
+
+        rows.push([`:${numbers[i]}:`, heroEmote, skillEmote, skill.Frequency]);
+        i++;
       });
 
-      const bonuses = [];
-      for (const key in attributes) {
-        if (attributes[key].count >= 2) {
-          bonuses.push(
-            `${attributes[key].emote} ${(attributes[key].count - 1) * 10}%`
-          );
-        }
-      }
-
-      const halfIndex = Math.ceil(data.Heroes.length / 2);
+      const halfIndex = Math.ceil(rows.length / 2);
       embed.addFields([
         {
-          name: "Heroes",
-          value: data.Heroes.slice(0, halfIndex)
-            .map(
-              (h) =>
-                `${h.HeroClassRead.DiscordEmote}${h.AttributeTypeRead.DiscordEmote} ${h.DiscordEmote} ${h.DisplayName}`
-            )
+          name: "Frequency",
+          value: rows
+            .slice(0, halfIndex)
+            .map((row) => `` + row.join(" "))
             .join("\n"),
           inline: true,
         },
         {
           name: "\u200b",
-          value: data.Heroes.slice(halfIndex)
-            .map(
-              (h) =>
-                `${h.HeroClassRead.DiscordEmote}${h.AttributeTypeRead.DiscordEmote} ${h.DiscordEmote} ${h.DisplayName}`
-            )
+          value: rows
+            .slice(halfIndex)
+            .map((row) => `` + row.join(" "))
             .join("\n"),
           inline: true,
         },
+      ]);
+    }
+
+    if (data.Formation) {
+      const rows = [];
+      for (var y = 0; y < 5; y++) {
+        rows[y] = [];
+        for (var x = 0; x < 5; x++) {
+          rows[y][x] = "<:blanksquare:1044298072940875816>";
+        }
+      }
+      const keys = Object.keys(data.Formation);
+      keys.map((key) => {
+        const hero = data.Heroes.filter((hero) => {
+          return hero.Code === key;
+        });
+        rows[data.Formation[key][0]][data.Formation[key][1]] =
+          hero[0].DiscordEmote;
+      });
+      embed.addFields([
         {
-          name: "Bonus",
-          value: bonuses.length > 0 ? bonuses.join(" ") : "None",
+          name: "Formation",
+          value: rows.map((row) => `` + row.join(" ")).join("\n"),
           inline: true,
         },
-        {
-          name: "Pet",
-          value: data.HeroPetRead.Name ? data.HeroPetRead.Name : "None",
-        },
       ]);
+    }
 
-      if (data.Frequency) {
-        const numbers = [
-          "one",
-          "two",
-          "three",
-          "four",
-          "five",
-          "six",
-          "seven",
-          "eight",
-        ];
-        const rows = [];
-        let i = 0;
-        data.Frequency.Skills.forEach((skill) => {
-          const skillInfo = skill.Code.split(".");
-          const heroEmote = data.Heroes.find(
-            (h) => h.Code == skillInfo[0]
-          ).DiscordEmote;
-          let skillEmote = "";
-          switch (skillInfo[1]) {
-            case "s1":
-              skillEmote =
-                skill.Frequency != 0
-                  ? "<:s1:1044371160168661032>"
-                  : "<:s1_disabled:1044373922042347560>";
-              if (skillInfo[2] == "lb") {
-                skillEmote =
-                  skill.Frequency != 0
-                    ? "<:s1lb:1044371161334677544>"
-                    : "<:s1lb_disabled:1044373921211879424>";
-              }
-              break;
-            case "s2":
-              skillEmote =
-                skill.Frequency != 0
-                  ? "<:s2:1044371163113074779>"
-                  : " <:s2_disabled:1044373918867263508>";
-              if (skillInfo[2] == "lb") {
-                skillEmote =
-                  skill.Frequency != 0
-                    ? "<:s2lb:1044371162299367464>"
-                    : "<:s2lb_disabled:1044373920217833572>";
-              }
-              break;
-          }
-          skill.Frequency = parseFloat(skill.Frequency).toFixed(1);
-          if (skill.Heal) {
-            skill.Frequency = `${skill.Frequency * 100}%`;
-          }
+    let embeds = [];
 
-          rows.push([`:${numbers[i]}:`, heroEmote, skillEmote, skill.Frequency]);
-          i++;
-        });
-
-        const halfIndex = Math.ceil(rows.length / 2);
-        embed.addFields([
-          {
-            name: "Frequency",
-            value: rows
-              .slice(0, halfIndex)
-              .map((row) => `` + row.join(" "))
-              .join("\n"),
-            inline: true,
-          },
-          {
-            name: "\u200b",
-            value: rows
-              .slice(halfIndex)
-              .map((row) => `` + row.join(" "))
-              .join("\n"),
-            inline: true,
-          },
-        ]);
-      }
-
-      if (data.Formation) {
-        const rows = [];
-        for (var y = 0; y < 5; y++) {
-          rows[y] = [];
-          for (var x = 0; x < 5; x++) {
-            rows[y][x] = "<:blanksquare:1044298072940875816>";
-          }
-        }
-        const keys = Object.keys(data.Formation);
-        keys.map((key) => {
-          const hero = data.Heroes.filter((hero) => {
-            return hero.Code === key;
-          });
-          rows[data.Formation[key][0]][data.Formation[key][1]] =
-            hero[0].DiscordEmote;
-        });
-        embed.addFields([
-          {
-            name: "Formation",
-            value: rows.map((row) => `` + row.join(" ")).join("\n"),
-            inline: true,
-          },
-        ]);
-      }
-
-      let embeds = [];
-
-      if (!refreshImage) {
-        embeds.push(embed);
-        return {
-          embeds: embeds,
-        };
-      }
-
+    if (!refreshImage) {
       embeds.push(embed);
       return {
         embeds: embeds,
       };
     }
+
+    embeds.push(embed);
+    return {
+      embeds: embeds,
+    };
   },
 };
