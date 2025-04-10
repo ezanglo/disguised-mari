351a6ffae678283849dde7afb36e1fec6a104e4a
ezraanglo <dev.ezraanglo@gmail.com>
2025-04-10 14:22:47 +0800

    Added shutdown message to interaction

--- commands/core/support.js
+++ commands/core/support.js
@@ -6,59 +6,22 @@ module.exports = {
     .setDescription("Show how you can support Disguised Mari"),
   async execute(interaction) {
 
-      const developers = [
-          "Ezwa#3117",
-          "Aonyx#7851",
-          "FallenTaco#0120",
-          "jocker#2022",
-          "シャイロ#2182",
-      ];
-
-      const embed = new EmbedBuilder()
-          .setTitle("Disguised Mari is shutting down...")
-          .setThumbnail(client.user.displayAvatarURL({ size: 1024, dynamic: true }))
-          .addFields([
-              {
-                  name: "Announcement",
-                  value: `> Hi everyone! Thanks a lot for using Disguised Mari! If you want to know more, please read this announcement https://discord.com/channels/857706068292665394/857768768029458432/1359745226478059550`,
-              },
-              {
-                  name: "Project is now open-sourced",
-                  value: `> I have open-sourced the project, if you want to learn how it's made or if you want to make your own 
-                  > Here is the actual bot's code https://github.com/ezanglo/disguised-mari 
-                  > Here is the new admin portal that I was working on https://github.com/ezanglo/disguised-mari-bot`,
-              },
-              {
-                  name: "Contact me",
-                  value: "> if you have any questions, you can DM me @ezwaa or contact me via https://ezraanglo.com",
-              },
-              {
-                  name: "Where can I find guides now????",
-                  value: "> You can join the official discord server http://discord.gg/grandchase OR You can refer to this GCDC Community Spreadsheet for all the information you need going forward. https://docs.google.com/spreadsheets/d/1FU4RI2MMvSQkO0k4c4IxgwY-hx2YFKNBIfhsXT4uC-I/",
-              },
-          ])
-          .setImage(
-              `https://media.discordapp.net/attachments/857764145298145291/1359768122147733524/bye.png?ex=67f8ae3f&is=67f75cbf&hm=7d650241b1e931ae693acab041b4dbc024c3fc593c09f72975c3b29bd00cfc52&=&format=webp&quality=lossless`
-          ).setFooter({
-              text: `Big thanks to the developers: ${developers.join(" | ")}`,
-          });
-
-    // const embed = new EmbedBuilder()
-    //   .setTitle("Support Disguised Mari")
-    //   .setThumbnail(client.user.displayAvatarURL({ size: 1024, dynamic: true }))
-    //   .addFields([
-    //     {
-    //       name: "\u200b",
-    //       value: `> *Hey there! Thanks a lot for considering supporting Disguised Mari! Your contribution will go a long way in helping the bot continue to provide valuable guides and information to chasers! We really appreciate your help and want to let you know that it means a lot to us. Every little bit counts, and your support will help Disguised Mari keep growing and improving. Thanks again for considering this opportunity to help us out!*`,
-    //     },
-    //     {
-    //       name: "\u200b",
-    //       value: "☕ https://ko-fi.com/disguisedmari",
-    //     },
-    //   ])
-    //   .setImage(
-    //     `https://media.discordapp.net/attachments/857764145298145291/1104517767262126171/image.png`
-    //   );
+    const embed = new EmbedBuilder()
+      .setTitle("Support Disguised Mari")
+      .setThumbnail(client.user.displayAvatarURL({ size: 1024, dynamic: true }))
+      .addFields([
+        {
+          name: "\u200b",
+          value: `> *Hey there! Thanks a lot for considering supporting Disguised Mari! Your contribution will go a long way in helping the bot continue to provide valuable guides and information to chasers! We really appreciate your help and want to let you know that it means a lot to us. Every little bit counts, and your support will help Disguised Mari keep growing and improving. Thanks again for considering this opportunity to help us out!*`,
+        },
+        {
+          name: "\u200b",
+          value: "☕ https://ko-fi.com/disguisedmari",
+        },
+      ])
+      .setImage(
+        `https://media.discordapp.net/attachments/857764145298145291/1104517767262126171/image.png`
+      );
     interaction.editReply({ embeds: [embed] });
   },
 };
